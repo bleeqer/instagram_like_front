35d9118e13c4009c59b3e1b6e93057ee4a03c35a
bleeq <bleeqtheproducer@gmail.com>
2022-11-08 23:12:11 +0900

    style: added stories

---
+++ data/users.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export const USERS = [
+    {
+        user: 'user1',
+        image: 'https://picsum.photos/201'
+    },
+    
+    {
+        user: 'user2',
+        image: 'https://picsum.photos/202'
+    },
+    
+    {
+        user: 'user3',
+        image: 'https://picsum.photos/203'
+    },
+    
+    {
+        user: 'user4',
+        image: 'https://picsum.photos/204'
+    },
+    
+    {
+        user: 'user5',
+        image: 'https://picsum.photos/205'
+    },
+    
+    {
+        user: 'user6',
+        image: 'https://picsum.photos/206'
+    },
+    
+    {
+        user: 'user7',
+        image: 'https://picsum.photos/207'
+    },
+    
+    {
+        user: 'user8',
+        image: 'https://picsum.photos/208'
+    },
+    
+    {
+        user: 'user9',
+        image: 'https://picsum.photos/209'
+    }
+]
+
